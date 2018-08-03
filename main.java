f2dbd5155df7c5e4851cd0bfa402b32b4e08c191
Rietzschie <42061560+Rietzschie@users.noreply.github.com>
2018-08-03 13:23:33 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main
+{
+	
+	public static void main(String[] args)
+    {
+    	 /* Erzeugung eines neuen Dialoges */
+         // Wir setzen die Breite auf 450 und die Höhe auf 300 Pixel
+         meinJDialog.setSize(450,300);
+         // Zur Veranschaulichung erstellen wir hier eine Border
+         Border bo = new LineBorder(Color.DARK_GRAY);
+         }
+         }
